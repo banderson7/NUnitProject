8c9297621a2b340757b83ad70f3ccb6c54198aa4
Bill Anderson <billanderson887@gmail.com>
2019-11-13 20:15:32 -0600

    basic text tests

--- NUnitProject/Model/LandingPage.cs
+++ NUnitProject/Model/LandingPage.cs
@@ -1,7 +1,50 @@
 ﻿using System;
+using OpenQA.Selenium;
+
 namespace NUnitProject.Model
 {
     public class LandingPage
     {
+        private IWebDriver driver;
+
+        private By header = By.TagName("h1");
+        private By subHeader = By.ClassName("lead");
+        private By signUpButton = By.CssSelector("a[class='btn btn-lg btn-info mr-2']");
+        private By loginButton = By.CssSelector("a[class='btn btn-lg btn-light']");
+
+        public LandingPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public String GetHeaderText()
+        {
+            return driver.FindElement(header).Text;
+        }
+
+        public String GetSubHeaderText()
+        {
+            return driver.FindElement(subHeader).Text;
+        }
+
+        public String GetSignUpButtonText()
+        {
+            return driver.FindElement(signUpButton).Text;
+        }
+
+        public String GetLoginButtonText()
+        {
+            return driver.FindElement(loginButton).Text;
+        }
+
+        public void ClickSignUp()
+        {
+            driver.FindElement(signUpButton).Click();
+        }
+
+        public void ClickLogin()
+        {
+            driver.FindElement(loginButton).Click();
+        }
     }
 }
--- NUnitProject/Tests/LandingPageTests.cs
+++ NUnitProject/Tests/LandingPageTests.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using NUnit.Framework;
+using NUnitProject.Model;
 
 namespace NUnitProject.Tests
 {
     public class LandingPageTests : BaseTest
     {
         [Test]
-        public void Test1()
+        public void Header_HasExpectedText()
         {
-            
+            const string expectedHeader = "NQA Social Media Test";
+
+            LandingPage landingPage = new LandingPage(driver);
+
+            Assert.AreEqual(expectedHeader, landingPage.GetHeaderText());
+        }
+
+        [Test]
+        public void SubHeader_HasExpectedText()
+        {
+            const string expectedSubHeader = "Create a profile, share posts and test";
+
+            LandingPage landingPage = new LandingPage(driver);
+
+            Assert.AreEqual(expectedSubHeader, landingPage.GetSubHeaderText());
+        }
+
+        [Test]
+        public void SignUpButton_HasExpectedText()
+        {
+            const string expectedLabel = "Sign Up";
+
+            LandingPage landingPage = new LandingPage(driver);
+
+            Assert.AreEqual(expectedLabel, landingPage.GetSignUpButtonText());
+        }
+
+        [Test]
+        public void LoginButton_HasExpectedText()
+        {
+            const string expectedLabel = "Login";
+
+            LandingPage landingPage = new LandingPage(driver);
+
+            Assert.AreEqual(expectedLabel, landingPage.GetLoginButtonText());
         }
     }
 }
